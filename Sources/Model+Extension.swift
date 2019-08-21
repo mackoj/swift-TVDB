50281a7155cdca43a34afefd6e856be80934ee2e
Jeffrey Macko <github.jm@macko.fr>
2019-08-21 11:42:53 +0200

    Better debugging

---
+++ Sources/Model+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Model+Extension.swift
+//  TVDBSwiftUI
+//
+//  Created by Jeffrey Macko on 20/08/2019.
+//  Copyright © 2019 Jeffrey Macko. All rights reserved.
+//
+
+import Foundation
+
+extension TVDBEpisode : Identifiable { }
+extension TVDBBasicEpisode : Identifiable { }
+extension TVDBLanguage : Identifiable { }
+extension TVDBSeries : Identifiable { }
+extension TVDBSeriesActorsData : Identifiable { }
+extension TVDBSeriesImageQueryResult : Identifiable { }
+extension TVDBSeriesSearchResult : Identifiable { }
+extension TVDBUpdate : Identifiable { }
